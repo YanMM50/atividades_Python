a5c410a0f889151a285822b3b8f3188cd694b6f4
YanMM50 <yanmendes50@gmail.com>
2024-08-16 18:14:14 +0000

    Comando iniciais.

---
+++ 1. Tipos primitivos/Tipagem dinamica.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("cls || clear") # limpa o ternimal.
+
+# Tipagem dinâmica.
+
+nome = "Yan"
+idade = 23
+altura = 1.70
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
---
+++ 1. Tipos primitivos/Tipagem estatica.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("cls || clear") # limpa o ternimal.
+
+# Tipagem estática
+nome : str # Cadeia
+idade : int # inteiro
+altura : float # real
+
+nome = "Yan"
+idade = 23
+altura = 1.70
+
+print(f"Nome: {nome}") 
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
