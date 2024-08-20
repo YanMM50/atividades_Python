89670087d48cac45e354664147d7298f4e2be34b
YanMM50 <yanmendes50@gmail.com>
2024-08-20 18:57:15 +0000

    aula

---
+++ 1. Tipos primitivos/Solicitando dados.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls || clear")
+
+# Solicitando dados.
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: ")) 
+altura = float (input("Digite sua altura: "))
+
+# Exibindo dados.
+print(f"Nome:  {nome}.")
+print(f"Idade:  {idade} anos.")
+print(f"Altura: {altura} Metros.")
+#print(f"Nome: {nome}, idade:  {idade} anos , Altura: {altura} Metros.")
