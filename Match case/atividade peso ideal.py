4c212286ca97b57ba4259e7965bf82736c83b24c
YanMM50 <yanmendes50@gmail.com>
2024-08-23 18:43:05 +0000

    atividade

--- Match case/atividade peso ideal.py
+++ Match case/atividade peso ideal.py
@@ -1,12 +1,14 @@
 import os
 os.system("cls || clear")
 
+peso_ideal = 0
+
 print("""\n
 1- Masculino (M)
 2- Feminino (F)            """)
 
-genero = input("Digite seu genero: ")
-altura = float(input("Digite sua altura: "))
+genero = input("Digite seu genero (M ou F): ").upper() #.upper() faz com que as letras se tornem maiusculas.
+altura = float(input("Digite sua altura: "))            #.lower() faz o contrario.
 
 match(genero):
     case "M":
@@ -15,3 +17,7 @@ match(genero):
     case "F":
         peso_ideal = (62.1 * altura) - 44.7
         print(f"\nSeu peso ideal seria: {peso_ideal: .2f}")
+    case _:
+        print("Opção inválida.")
+        
+
