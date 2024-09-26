09ba14551eaa045dddade013e614011f10843e4c
YanMM50 <yanmendes50@gmail.com>
2024-09-26 16:53:12 +0000

    salvar

---
+++ Lista - Vetores - array/teste de versionamento.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls || clear")
+
+print("Olá, mundo!")
+
+# Identificar arquivos para alvar (commit)
+# git status
+
+# Adicionar arquivos para salvar (commit)
+# git add .
+
+# Realizando salvamento (commit)
+# git commit -m "Mensagem"
+
+# atualizar seu repositório local em relação ao repositorio remoto. 
+# git pull
+
+# Envia seu código para o reposirório remoto (Github)
+# git push
