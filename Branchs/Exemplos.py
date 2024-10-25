f9a572cd6babffa129f2ad84f10ac4714ab21d66
YanMM50 <yanmendes50@gmail.com>
2024-10-25 23:08:31 +0000

    salvar

---
+++ Branchs/Exemplos.py
@@ -0,0 +1,17 @@
+import os
+os.system("clss || clear")
+
+# git branch
+# git checkout -b = criar
+# git branch -d = delete
+# fix = corrigir
+# feature = criar  
+# git merge = juntar
+# git commit --amend -m = trocar nome do commit
+
+
+print("Adicionando funcionalidade: função inss.")
+
+def inss():
+    # Considere feito.
+    pass
